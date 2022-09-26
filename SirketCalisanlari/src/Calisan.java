85b45d99f0b95f138f69caab934277aff9165885
Mustafacoban <mustafa.coban@std.hku.edu.tr>
2022-09-26 15:33:10 +0300

    SirketApp

---
+++ SirketCalisanlari/src/Calisan.java
@@ -0,0 +1,56 @@
+
+public class Calisan {
+	
+	private int id;
+	private String ad;
+	private String soyad;
+	private String department;
+	private String maas;
+	public Calisan(int id, String ad, String soyad, String department, String maas) {
+		this.id = id;
+		this.ad = ad;
+		this.soyad = soyad;
+		this.department = department;
+		this.maas = maas;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAd() {
+		return ad;
+	}
+	public void setAd(String ad) {
+		this.ad = ad;
+	}
+	public String getSoyad() {
+		return soyad;
+	}
+	public void setSoyad(String soyad) {
+		this.soyad = soyad;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getMaas() {
+		return maas;
+	}
+	public void setMaas(String maas) {
+		this.maas = maas;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
